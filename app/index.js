40d2a55aeb4f3a5e6248d4e54febe7e5e4568212
J. Arturo Covarrubias <a.covar@gmail.com>
2014-05-29 15:38:18 -0500

    Added changed files

--- app/index.js
+++ app/index.js
@@ -4,20 +4,54 @@ var aws = require('aws-sdk'),
 	http= require('http'),
 	fs=require('fs'),
 	path= require('path'),
-	express=require('express');
+	express=require('express'),
+	bodyParser = require('body-parser'),
+	multipart=require('connect-multiparty');
+
 //NEW EXPRESS APP
 var app = new express();
+// MIDDLEWARE FOR MULTIPART
+var  multipartMiddleware= multipart();
+//SET THE JADE VIEWS
+app.set('views',__dirname + '/views');
+app.set('view engine','jade');
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname,'public')));
+
 //PATH JSON FILE
 var config_path=path.join(__dirname,'../auth.json');
+
 //CONFIG AWS
 aws.config.loadFromPath(config_path);
+
 // AWS OBJECT
 var s3 = new aws.S3();
+
 // AWS list Buckets
 app.get('/buckets',function(req,res) {
 	s3.listBuckets(function(err,data) {
 		res.json(data);
 	});
+});
+
+// AWS Upload File 
+app.get('/upload',function(req,res) {
+	res.render('upload');
+});
+
+app.post('/upload',multipartMiddleware,function(req,res) {
+	// console.log(req);
+	console.log(req.files);
+	var s3request = {
+		Body: fs.readFileSync(req.files.uploadedFile.path),
+		Bucket:"acovar.email.upload",
+		Key:req.files.uploadedFile.name
+	};
+
+	s3.putObject(s3request,function(err, data) {
+		res.render('upload',{done: true});
+	});
+
 });
 
 module.exports=app;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require('http'),
 
-
+	repl=require('repl'),
 	app = require('./app');
 
 
 http.createServer(app).listen(3000, function(){
   console.log('Express server listening on port ' + 3000);
 });
+
+var promt = repl.start({prompt: 'aws> '});
